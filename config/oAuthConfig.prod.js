575a082735f1dd7acca96caafc2fb610bb6fb8ab
David Rosenberg <dsr0116@yahoo.com>
2017-10-18 19:32:33 -0400

    added facebook cred

--- config/oAuthConfig.prod.js
+++ config/oAuthConfig.prod.js
@@ -1,7 +1,7 @@
 var ids = {
   facebook: {
-    clientID: 'get your own',
-    clientSecret: 'get your own'
+    clientID: '125465694765608',
+    clientSecret: '76d533cd8b5692665df3a163173e1d63'
   }
 }
 // please get your own IDs. :)
